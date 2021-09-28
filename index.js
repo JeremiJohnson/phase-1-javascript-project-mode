ee03bb254319c8b40b1ffb78bb8d6be6cecb7c13
Jeremi <10thlettermusic@gmail.com>
2021-09-28 15:10:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,111 +1,21 @@
-fetch('http://localhost:3000/answers')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-
-const answerText = ["You can count on it!", "Dont hold your breath", "You dont want to know", "Absolutely!", "Its not looking good", "Actually...this one i'm unsure about...", "Do you really want the answer to that...", "100% yes!", "Not a chance!", "I wouldn't get my hopes up...", "I can't with you right now...come back later", "Most definitely!", "I hate to be the bearer of bad news buuuut.....", "Most likely", "Not a chance in hell", "Come on...Really?? Youre gonna ask me...The Void...gaterkeeper of infinite knowlede and wisdom...a question like that!"]
-
-
-type = "text/Javascript";
-function generate_random(max_number) {
-  return Math.round(Math.random() * max_number);
-}
-
-
-let button = document.querySelector("button");
+let answers 
+ let button = document.querySelector("button");
 let answer = document.querySelector("#answer");
 
-button.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", () => {
+ fetch('http://localhost:3000/answers')
+  .then(response => response.json())
+  .then(answers => {
+     button.addEventListener("click", () => {
+      const index = Math.floor(Math.random() * answers.length) - 1
+      console.log(answers[index] )
+      
+      })
+  })
 
-  let randomNumber = generate_random(16);
-  let answerText = "";
-
-  if (randomNumber == 0) {
-
-    answerText = "You can count on it!";
-  }
-  else if (randomNumber == 1) {
-
-    answerText = "Dont hold your breath";
-  }
-  else if (randomNumber == 2) {
-
-    answerText = "You dont want to know";
-  }
-
-  else if (randomNumber == 3) {
-
-    answerText = "Absolutely!";
-  }
-
-  else if (randomNumber == 4) {
-
-    answerText = "Its not looking good";
-  }
-
-  else if (randomNumber == 5) {
-
-    answerText = "Actually...this one i'm unsure about...";
-  }
-
-  else if (randomNumber == 6) {
-
-    answerText = "Do you really want the answer to that...";
-  }
-
-  else if (randomNumber == 7) {
-
-    answerText = "100% yes!";
-  }
-
-  else if (randomNumber == 8) {
-
-    answerText = "Not a chance!";
-  }
-
-  else if (randomNumber == 9) {
-
-    answerText = "You got this!";
-  }
-
-  else if (randomNumber == 10) {
-
-    answerText = "I wouldn't get my hopes up...";
-  }
-
-  else if (randomNumber == 11) {
-
-    answerText = "I can't with you right now...come back later";
-  }
-
-  else if (randomNumber == 12) {
-
-    answerText = "Most definitely!";
-  }
-
-  else if (randomNumber == 13) {
-
-    answerText = "I hate to be the bearer of bad news buuuut.....";
-  }
+})
 
 
-  else if (randomNumber == 14) {
-
-    answerText = "Most likely";
-  }
-
-  else if (randomNumber == 15) {
-
-    answerText = "Not a chance in hell";
-  }
-
-  else if (randomNumber == 16) {
-
-    answerText = "Come on...Really?? Youre gonna ask me...The Void...gaterkeeper of infinite knowlede and wisdom...a question like that!";
-  }
-
-  answer.innerHTML = answerText;
-});
 
 let a1 = document.getElementById("flora");
 let a2 = document.getElementById("vacuum");
